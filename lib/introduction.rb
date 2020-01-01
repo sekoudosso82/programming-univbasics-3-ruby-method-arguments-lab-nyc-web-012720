7c47319e07c0b419f572759744f40959d59c574e
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-01 23:51:49 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,7 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end 
 
-def introduction_with_language_optional (name, language = "x")
+def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end 
 
